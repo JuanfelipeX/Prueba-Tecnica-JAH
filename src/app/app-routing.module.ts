64c749f6295b1eec41c9687304963a806545a37b
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-06-15 12:52:28 -0500

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { TercerosComponent } from './components/terceros/terceros.component';
+
+const routes: Routes = [
+  { path: 'iniciar-sesion', component: IniciarSesionComponent },
+  { path: 'tercero', component: TercerosComponent },
+  { path: '', redirectTo: 'iniciar-sesion', pathMatch: 'full' },
+  { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TercerosComponent } from './components/terceros/terceros.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IniciarSesionComponent,
+    ErrorNoEncontradoComponent,
+    TercerosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgbModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
